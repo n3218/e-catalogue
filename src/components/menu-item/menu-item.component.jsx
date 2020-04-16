c5bc65cd619ffac559e87d6a4b0a052a0f368e1b
Natalia Shport <shport.natalia@gmail.com>
2020-04-16 15:15:30 -0700

    added links to menu-items using withRouter

--- src/components/menu-item/menu-item.component.jsx
+++ src/components/menu-item/menu-item.component.jsx
@@ -1,8 +1,10 @@
 import React from 'react';
 import './menu-item.styles.scss';
+import { withRouter } from 'react-router-dom';
 
-const MenuItem = ({ title, id, imageUrl, size }) => (
-    <div className={`${size} menu-item`}>
+const MenuItem = ({ title, id, imageUrl, history, size, linkUrl, match }) => (
+    <div className={`${size} menu-item`} 
+        onClick={() => history.push(`${match.url}${linkUrl}`)} >
         <div className='background-image'
             style={{ backgroundImage: `url(${imageUrl})` }} ></div>
         <div className='content'>
@@ -12,4 +14,4 @@ const MenuItem = ({ title, id, imageUrl, size }) => (
     </div>
 )
 
-export default MenuItem
+export default withRouter(MenuItem);
