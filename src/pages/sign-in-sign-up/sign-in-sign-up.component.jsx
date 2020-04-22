b0ce3fb276b08ae1286ce9f92ce2e874c6945624
Natalia Shport <shport.natalia@gmail.com>
2020-04-22 00:07:52 -0700

    Sign In form has bben created

---
+++ src/pages/sign-in-sign-up/sign-in-sign-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import './sign-in-sign-up.styles.scss';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+const SignInAndSignUpPage = () => (
+    <div className='sign-in-sign-up'>
+        SIGN IN AND SIGN UP
+        <SignIn />
+    </div>
+
+)
+
+export default SignInAndSignUpPage;
+
