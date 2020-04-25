df935b4266863145c29e2d93e559f304f1b39f56
Natalia Shport <shport.natalia@gmail.com>
2020-04-25 00:42:29 -0700

    Created CartItem component and connected with Cart DropDown component

---
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,24 @@
+import React from 'react';
+import { connect } from 'react-redux';
+
+import './cart-dropdown.styles.scss';
+import CustomButton from '../custom-button/custom-button.component';
+import CartItem from '../cart-item/cart-item.component';
+
+
+const CartDropdown = ({ cartItems }) => (
+    <div className='cart-dropdown'>
+        <div className='cart-items'>
+            {cartItems.map(item => (
+                <CartItem key={item.id} item={item} />
+            ))}
+        </div>
+        <CustomButton>GO TO CHECKOUT</CustomButton>
+    </div>
+)
+
+const mapStateToProps = ({ cart: { cartItems } }) => ({
+    cartItems
+})
+
+export default connect(mapStateToProps)(CartDropdown);
--- src/components/header/header.component.jsx
+++ src/components/header/header.component.jsx
@@ -6,7 +6,7 @@ import './header.styles.scss';
 import { ReactComponent as Logo } from '../../assets/crown.svg';
 import { auth } from '../../firebase/firebase.utils';
 import CartIcon from '../cart-icon/cart-icon.component';
-import CartDropdown from '../cart-dropdown/cart-dropdown.coponent';
+import CartDropdown from '../cart-dropdown/cart-dropdown.component';
 
 
 const Header = ({ currentUser, hidden }) => (
