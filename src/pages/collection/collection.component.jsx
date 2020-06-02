4e0a75e45859e055bf2b780926b96f021692535e
Natalia Shport <shport.natalia@gmail.com>
2020-06-01 17:27:58 -0700

    Using CollectionsContext with UseContext

--- src/pages/collection/collection.component.jsx
+++ src/pages/collection/collection.component.jsx
@@ -1,27 +1,22 @@
-import React from 'react';
+import React, {useContext} from 'react';
 import './collection.styles.scss';
 import CollectionItem from '../../components/collection-item/collection-item.component';
-import CollectionsContext from '../../contexts/collections/collections.context';
+import CollectionContext from '../../contexts/collections/collections.context';
 
 
 const CollectionPage = ({ match }) => {
+    const collections = useContext(CollectionContext)
+    const collection = collections[match.params.collectionId]
+    const { title, items } = collection
     return (
-        <CollectionsContext.Consumer>
-            {collections => {
-                const collection = collections[match.params.collectionId]
-                const { title, items } = collection;
-                return (
-                    <div className='collection-page'>
-                        <h2 className='title'>{title}</h2>
-                        <div className='items'>
-                            {items.map((item) =>
-                                <CollectionItem key={item.id} item={item} />
-                            )}
-                        </div>
-                    </div>
-                )
-            }}
-        </CollectionsContext.Consumer>
+        <div className='collection-page'>
+            <h2 className='title'>{title}</h2>
+            <div className='items'>
+            { items.map((item) =>
+                <CollectionItem key={item.id} item={item} />
+            )}
+            </div>
+        </div>
     )
 }
 
