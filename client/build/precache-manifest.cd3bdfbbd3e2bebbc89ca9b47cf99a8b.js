a2b8a0792f3b8894227718c12bcf47c8ee206f8b
Natalia Shport <shport.natalia@gmail.com>
2020-06-17 01:58:31 -0700

    Fixes on serviceWorker

---
+++ client/build/precache-manifest.cd3bdfbbd3e2bebbc89ca9b47cf99a8b.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd71b827ea61d01f8e59f7079332ab9d",
+    "url": "./index.html"
+  },
+  {
+    "revision": "2e69f852a8824a807f1f",
+    "url": "./static/css/4.24efc72f.chunk.css"
+  },
+  {
+    "revision": "ec2a700d9ff2517251d0",
+    "url": "./static/css/5.22627aeb.chunk.css"
+  },
+  {
+    "revision": "6144b914b2da8edd38b6",
+    "url": "./static/css/6.c9228d6e.chunk.css"
+  },
+  {
+    "revision": "ca820284123a86babed2",
+    "url": "./static/css/7.1078e032.chunk.css"
+  },
+  {
+    "revision": "df32be5aaa1c6b0d06e9",
+    "url": "./static/css/8.287fa6fa.chunk.css"
+  },
+  {
+    "revision": "c2bf015790a1fc5cc656",
+    "url": "./static/css/9.b23be3d8.chunk.css"
+  },
+  {
+    "revision": "5eb98a3c1383046de3e7",
+    "url": "./static/css/main.53e4f450.chunk.css"
+  },
+  {
+    "revision": "86e391486e827787b440",
+    "url": "./static/js/2.432240eb.chunk.js"
+  },
+  {
+    "revision": "6331f70a3ea8aafd2e3fca1343e5ed47",
+    "url": "./static/js/2.432240eb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e66d52c101411254c1b",
+    "url": "./static/js/3.358dcc1e.chunk.js"
+  },
+  {
+    "revision": "2e69f852a8824a807f1f",
+    "url": "./static/js/4.d187c338.chunk.js"
+  },
+  {
+    "revision": "ec2a700d9ff2517251d0",
+    "url": "./static/js/5.b8411b04.chunk.js"
+  },
+  {
+    "revision": "6144b914b2da8edd38b6",
+    "url": "./static/js/6.551df5c9.chunk.js"
+  },
+  {
+    "revision": "ca820284123a86babed2",
+    "url": "./static/js/7.9af586d1.chunk.js"
+  },
+  {
+    "revision": "df32be5aaa1c6b0d06e9",
+    "url": "./static/js/8.8950ffc1.chunk.js"
+  },
+  {
+    "revision": "c2bf015790a1fc5cc656",
+    "url": "./static/js/9.36c210bf.chunk.js"
+  },
+  {
+    "revision": "5eb98a3c1383046de3e7",
+    "url": "./static/js/main.6d08f140.chunk.js"
+  },
+  {
+    "revision": "f9cdb258c3f0d84c2cf6",
+    "url": "./static/js/runtime-main.6307d4a3.js"
+  },
+  {
+    "revision": "30f0707118fb960665491424c115ac44",
+    "url": "./static/media/cart.30f07071.svg"
+  },
+  {
+    "revision": "0d56b8100ee91e9deaf136f3ff318ed1",
+    "url": "./static/media/flowers.0d56b810.svg"
+  }
+]);
