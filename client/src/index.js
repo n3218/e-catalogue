b44f838bffae405f2f1e1a2f02d47efd6a254d03
Natalia Shport <shport.natalia@gmail.com>
2020-08-20 14:54:10 -0700

    Looking fot manifest.json

--- client/src/index.js
+++ client/src/index.js
@@ -6,7 +6,7 @@ import { PersistGate } from "redux-persist/integration/react"
 import "./index.css"
 import App from "./App"
 import { store, persistor } from "./redux/store"
-import * as serviceWorker from "./serviceWorker"
+import * as serviceWorker from "./service-worker"
 
 ReactDOM.render(
   <Provider store={store}>
@@ -19,5 +19,4 @@ ReactDOM.render(
   document.getElementById("root")
 )
 
-// serviceWorker.unregister()
 serviceWorker.register()
--- client/sw-precache-config.js
+++ client/sw-precache-config.js
@@ -2,5 +2,5 @@ module.exports = {
   stripPrefix: "build/",
   staticFileGlobs: ["build/*.html", "build/manifest.json", "build/static/**/!(*map*)"],
   dontCacheBustUrlsMatching: /\.\w{8}\./,
-  swFilePath: "build/serviceWorker.js"
+  swFilePath: "build/service-worker.js"
 }
